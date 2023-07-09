83eed6a333b4933ae20d1e7d28afab9c03c012ff
Yi Ming <ofseed@foxmail.com>
2023-07-09 13:48:27 +0800

    fix: make `id` a string as jwt sub

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -79,6 +79,6 @@ async def login(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.id}, expires_delta=access_token_expires
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
