2c3d91d2bbffe0163bf6e985ca98ed52163ee6b0
Zeason <20301138@bjtu.edu.cn>
2023-07-14 11:54:23 +0800

    feat(crud.py): add requirement torch, complete method add record

--- app/routers/infer.py
+++ app/routers/infer.py
@@ -2,6 +2,8 @@ from app import schemas
 from app.dependencies import get_current_active_user, get_db
 from app.crud import *
 
+from datetime import date, time, datetime
+
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 
 from sqlalchemy.orm import Session
@@ -10,7 +12,8 @@ from typing import Annotated
 
 router = APIRouter(prefix="/infer", tags=['infer'])
 
-#def stock_and_process
+def infer(db: Session, user_id: int):
+    pass
 
 @router.post("/send-status")
 async def receive_status(
@@ -19,8 +22,21 @@ async def receive_status(
     background_task: BackgroundTasks,
     db: Annotated[Session, Depends(get_db)]
 ):
+    record_number = get_users_record_number(db, current_user.id)
+    if 0 < record_number < 50:
+        last_record = get_last_record(db, current_user.id)
+        time_now = datetime.now()
+        time_latest = datetime.combine(last_record.date, last_record.time)
+        if (time_now - time_latest).seconds > 3600:
+            background_task.add_task(infer, db, current_user.id)
+        else:
+            create_user_record(db, user_record, current_user.id)
+    if record_number == 0:
+        create_user_record(db, user_record, current_user.id)
+    else:
+        background_task.add_task(infer, db, current_user.id)
 
-    return [{"fuck you": get_users_record_number(db, current_user.id)}]
+    return [{"msg": "success inject"}]
 
 
 
