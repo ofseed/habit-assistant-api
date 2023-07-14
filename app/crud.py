921f0aa21f8c580aa0dda4e1b67d1c8ead368b6d
Zeason <20301138@bjtu.edu.cn>
2023-07-14 11:15:12 +0800

    feat(crud.py): new a method get one user's latest record

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from app import models, schemas
@@ -10,6 +11,8 @@ def get_user(db: Session, user_id: int):
 def get_users_record_number(db: Session, user_id: int):
     return len(db.query(models.Record).filter(models.Record.owner_id == user_id).all())
 
+def get_last_record(db: Session, user_id: int):
+    return db.query(models.Record).filter(models.Record.owner_id == user_id).order_by(desc(models.Record.time)).first()
 
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
