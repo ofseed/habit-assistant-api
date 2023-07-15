dda4fe288b6339205fde7d97cc7c4cc8fbbea413
Zeason <20301138@bjtu.edu.cn>
2023-07-15 11:20:22 +0800

    feat(infer): complete latter part of infer, insert infer result to the sql

--- app/routers/infer.py
+++ app/routers/infer.py
@@ -7,7 +7,8 @@ from app.crud import *
 from app.models import Record
 from app.utils.ContextAwareness import ContextLSTM, ContextFormer
 
-from datetime import date, time, datetime
+from datetime import datetime
+import datetime as dt
 
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 
@@ -23,6 +24,7 @@ async def transform_records_to_torch(
     place_dict = {"食堂": [0, 0, 1, 0], "教室": [1, 0, 0, 0], "宿舍": [0, 1, 0, 0],
                   "其他": [0, 0, 0, 1]}
     all_embeddings = []
+    # to traverse each records, add it's parameter into list and convert list to tensor
     for record in records:
         one_embedding = [record.gyroscopeX, record.gyroscopeY, record.gyroscopeZ,
                          record.accelerateX, record.accelerateY, record.accelerateZ,
@@ -30,7 +32,7 @@ async def transform_records_to_torch(
                          record.time.second]
         one_embedding += place_dict[record.place]
         all_embeddings.append(one_embedding)
-
+    # convert form
     all_embeddings = np.array(all_embeddings).astype("float32")
     all_embeddings = np.expand_dims(all_embeddings, axis=0)
     all_embeddings = torch.Tensor(all_embeddings)
@@ -38,18 +40,40 @@ async def transform_records_to_torch(
     return all_embeddings
 
 
+async def create_users_status(
+        db: Session,
+        user_id: int,
+        status: str
+):
+    end = get_last_record(db, user_id).time
+    start = get_earliest_record(db, user_id).time
+    date = dt.date.today()
+
+    for_create = schemas.StatusCreate(start=start, end=end, date=date, statusS=status)
+    create_user_status(db, for_create, user_id)
+
+
 async def infer(
+        db: Session,
         records: Record,
         user_id: int,
         model: ContextLSTM
 ):
     status_dict = {5: "学习", 4: "通勤", 3: "睡觉", 2: "吃饭", 1: "运动", 0: "摸鱼"}
+
+    # to get infer
     records_tensor = await transform_records_to_torch(records)
     output = model(records_tensor)
     index = torch.argmax(output)
-    print(status_dict[int(index)])
+    status = status_dict[int(index)]
+
+    await create_users_status(db, user_id, status)
+
+    # to delete cache of users record
+    delete_users_record_all(db, user_id)
 
 
+# get data, judge that whether user's status should be infered or not
 @router.post("/send-status")
 async def receive_status(
         current_user: Annotated[schemas.User, Depends(get_current_active_user)],
@@ -61,6 +85,7 @@ async def receive_status(
     records = get_users_record_all(db, current_user.id)
     record_number = len(records)
 
+    # judge whether data need to be inferred, if his record number is  more than 6, deal it
     if 0 < record_number < 6:
         last_record = get_last_record(db, current_user.id)
         time_now = datetime.now()
@@ -74,7 +99,6 @@ async def receive_status(
     elif record_number == 0:
         create_user_record(db, user_record, current_user.id)
     else:
-        background_task.add_task(infer, records, current_user.id, model)
-        delete_users_record_all(db, current_user.id)
+        background_task.add_task(infer, db, records, current_user.id, model)
 
     return [{"msg": "success inject", "data": 0}]
