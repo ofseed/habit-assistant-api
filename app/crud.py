cda66f7ea8f258416782463777a24a7bb323f40c
Zeason <20301138@bjtu.edu.cn>
2023-07-15 09:45:54 +0800

    feat(crud): add method that delete users record that long long ago

--- app/crud.py
+++ app/crud.py
@@ -8,11 +8,18 @@ def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def delete_users_record_all(db: Session, user_id: int):
+    db.query(models.Record).filter(models.Record.owner_id == user_id).delete()
+    db.commit()
+
+
 def get_users_record_all(db: Session, user_id: int):
     return db.query(models.Record).filter(models.Record.owner_id == user_id).all()
 
+
 def get_last_record(db: Session, user_id: int):
     return db.query(models.Record).filter(models.Record.owner_id == user_id).order_by(desc(models.Record.time)).first()
+
 
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
