512165d30bac201531aa35748a13219ea1e94be9
Zeason <20301138@bjtu.edu.cn>
2023-07-16 16:41:24 +0800

    fix: modify dependency, to use new model

--- app/dependencies.py
+++ app/dependencies.py
@@ -55,7 +55,7 @@ async def get_current_user(
 
 
 async def get_classifier():
-    path = "app/utils/ContextLSTMBeta.pth"
+    path = "app/utils/ContextLSTM.pth"
     model = ContextAwareness.ContextLSTM(14, 6)
     model.load_state_dict(torch.load(path))
     model.eval()
